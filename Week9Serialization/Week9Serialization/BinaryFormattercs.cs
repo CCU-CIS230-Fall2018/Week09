f825ff4b0770009d9fe1833ebd6a16a970178790
Noah Jamison <njamison@students.ccu.edu>
2018-10-25 23:40:56 -0600

    Finished.

---
+++ Week9Serialization/Week9Serialization/BinaryFormattercs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace Week9Serialization
+{
+    class BinaryFormattercs : MyInterface
+    {
+        private List<Type> knownTypes;
+        public BinaryFormattercs(List<Type> types)
+        {
+            knownTypes = types;
+        }
+        public void Serialize<T>(string saveToPath, T myCsObj)
+        {
+            FileStream fs = new FileStream(saveToPath, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, knownTypes);
+            fs.Close();
+        }
+
+        public T Deserialize<T>(string createToPath)
+        {
+            FileStream fs = new FileStream(createToPath, FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            knownTypes = (List<Type>) formatter.Deserialize(fs);
+            return default(T);
+        }
+    }
+}
---
+++ Week9Serialization/Week9Serialization/JSONSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week9Serialization
+{
+    class JSONSerializer
+    {
+        // api.nuget.org was down when I attempted to download json.net
+        // See attached screenshot
+    }
+}
